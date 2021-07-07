e1d51bfbfa788774ea59e32d3f6c2cbb40aecbdc
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2021-07-07 22:44:02 +0600

    Added searh and clear button funtion and warning message
    search funtion isnt implemented yet, only the warning message.
    Instead of getting one line at a time, now will make a string first,
    Then append QplainText to display file text

--- UI/MainWindowUi.py
+++ UI/MainWindowUi.py
@@ -1,9 +1,9 @@
 import sys
 from PyQt5 import  QtWidgets as qtw
-from PyQt5 import  QtWidgets as qtw
+from PyQt5 import  QtCore as qtc
 from PyQt5 import  QtGui as qtg
-
-
+from PyQt5.QtGui import QTextCursor
+import time
 
 
 class MainWindowUi(qtw.QWidget):
@@ -20,7 +20,9 @@ class MainWindowUi(qtw.QWidget):
         self.search_button=qtw.QPushButton("Search")
         self.clear_button = qtw.QPushButton("Clear")
         self.display_result=qtw.QPlainTextEdit()
-        self.display_result.setReadOnly(True)
+        #self.display_result.setReadOnly(True)
+        self.display_result.centerOnScroll()
+        self.warning_label=qtw.QLabel()
 
 
         self.layout.addWidget(self.display_result,0,0,5,5)
@@ -29,26 +31,59 @@ class MainWindowUi(qtw.QWidget):
         self.layout.addWidget(self.line_number_after_input ,1, 7)
         self.layout.addWidget(self.search_button ,2, 6)
         self.layout.addWidget(self.clear_button, 2, 7)
+        self.layout.addWidget(self.warning_label, 3,6,2,2)
+
+
         self.setLayout(self.layout)
 
         self.upload_file_button.clicked.connect(self.open)
+        self.search_button.clicked.connect(self.search_words)
+        self.clear_button.clicked.connect(self.clear_all)
 
         self.showWindow()
 
+    def clear_all(self):
+        self.search_text_input.clear()
+        self.line_number_after_input.clear()
+
+    def show_warning(self):
+        self.warning_label.setText("Insert both search word and line number please")
+        self.warning_label.setWordWrap(True)
+        self.warning_label.setStyleSheet('color: red')
+        #self.warning_label.setVisible(True)
+    def search_words(self):
+        keyword=self.search_text_input.text()
+        line_number=self.search_text_input.text()
+        if not keyword or not line_number:
+           # pass
+            self.show_warning()
+        else:
+            pass
 
 
     def showWindow(self):
         self.show()
 
+
+    def  display_file(self,line):
+        # text_cursor=self.display_result.textCursor()
+        # text_cursor.movePosition(qtg.QTextCursor.End)
+        # text_cursor.insertText(line)
+        self.display_result.clear()
+
+        self.display_result.appendPlainText(line)
+
+
     def open(self):
         path = qtw.QFileDialog.getOpenFileName(self, '', '',
                                            'Text files (*.txt *log*)')
 
-        print(path[0])
-        self.display_result.clear()
-        with open(path[0],'r',encoding="utf8") as file:
-            for line in file:
-                self.display_result.appendPlainText(line)
+        if path[0]:
+             with open(path[0],'r',encoding="utf8") as file:
+                #for line in file:
+                    self.display_file(file.read())
+                    #print(line)
+                    #self.display_result.appendPlainText(line)
 
 
 
